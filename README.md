84b80f40119842c1cc8434556c7d4da129e4b9c1
moonti <tmmoond8@gmail.com>
2019-07-04 21:13:38 +0900

    frontend setup

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # graphql-yoga-react-apollo-ts-biolerplate
 
-yarn 1.15.2v
+> this is a sample project for using the GraphQL.
+
+## Project Stacks
+---
+### front-end 
+
+- React
+- TypeScript
+- Apollo
+- Styled Component
+
+### back-end
+- Node.js
+- TypeScript
+- Graphql-Yoga  
+
+
+
+## Quick Start
+---
--- backend/src/api/getFile/getFile.resolvers.ts
+++ backend/src/api/getFile/getFile.resolvers.ts
@@ -7,24 +7,15 @@ const projectRoot = path.basename(root);
 
 const getChildren = (parent: string) => {
   const files = readdirSync(parent);
-  return files.map(name => {
-    const filename = `${projectRoot}/${name}`;
-
-    if (fs.statSync(path.join(parent, name)).isDirectory()) {
-      return {
-        name: filename,
-        children: getChildren(path.resolve(parent, name))
-      }
-    } else {
-      return {
-        name: filename,
-        children: null
-      };
-    }
-  });
+  return files.map(name => ({
+    name: `${projectRoot}/${name}`,
+    children: fs.statSync(path.join(parent, name)).isDirectory()
+      ? getChildren(path.resolve(parent, name))
+      : null
+  }));
 }
 
-const fileTreeSet = () => {
+const fileTree = (function () {
   try {
     const children = getChildren(path.resolve(root, 'src'));
     return {
@@ -38,9 +29,7 @@ const fileTreeSet = () => {
       children: null
     }
   }
-};
-
-const fileTree = fileTreeSet();
+})();
 
 const resolvers = {
   Query: {
