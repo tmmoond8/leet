5c6bc5fe2aaa71aa1ba80367a88c6632ab34d679
moonti <tmmoond8@gmail.com>
2019-08-04 17:58:41 +0900

    #1 구글 시트에 연결해서 초성 데이터 초기화

--- backend/src/types/graph.d.ts
+++ backend/src/types/graph.d.ts
@@ -1,10 +1,15 @@
-export const typeDefs = ["type File {\n  name: String!\n  children: [File]\n  content: String!\n}\n\ntype Query {\n  GetFile: File\n  GetMessages: GetMessagesResponse!\n  GetUser: GetUserResponse!\n}\n\ntype GetMessagesResponse {\n  ok: Boolean!\n  error: String\n  messages: [Message]\n}\n\ntype GetUserResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype JoinChatResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  JoinChat(nickname: String!): JoinChatResponse!\n  SendMessage(text: String!): SendMessageResponse!\n}\n\ntype Subscription {\n  MessageSubscription: Message\n}\n\ntype SendMessageResponse {\n  ok: Boolean!\n  error: String\n  message: Message\n}\n\ntype Message {\n  id: Int!\n  user: User!\n  text: String!\n  createdAt: Float!\n}\n\ntype User {\n  id: Int!\n  nickname: String!\n}\n"];
+export const typeDefs = ["type File {\n  name: String!\n  children: [File]\n  content: String!\n}\n\ntype Query {\n  GetFile: File\n  GetMessages: GetMessagesResponse!\n  GetUser: GetUserResponse!\n  GetBasicInitial(level: Int!): GetQuizInitialResponse!\n}\n\ntype GetMessagesResponse {\n  ok: Boolean!\n  error: String\n  messages: [Message]\n}\n\ntype GetUserResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype JoinChatResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  JoinChat(nickname: String!): JoinChatResponse!\n  SendMessage(text: String!): SendMessageResponse!\n}\n\ntype Subscription {\n  MessageSubscription: Message\n}\n\ntype SendMessageResponse {\n  ok: Boolean!\n  error: String\n  message: Message\n}\n\ntype Message {\n  id: Int!\n  user: User!\n  text: String!\n  createdAt: Float!\n}\n\ntype User {\n  id: Int!\n  nickname: String!\n}\n\ntype QuizInitial {\n  quiz: String!\n  Syllables: [String]!\n}\n\ntype GetQuizInitialResponse {\n  ok: Boolean!\n  error: String\n  data: [QuizInitial]\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   GetFile: File | null;
   GetMessages: GetMessagesResponse;
   GetUser: GetUserResponse;
+  GetBasicInitial: GetQuizInitialResponse;
+}
+
+export interface GetBasicInitialQueryArgs {
+  level: number;
 }
 
 export interface File {
@@ -35,6 +40,17 @@ export interface GetUserResponse {
   ok: boolean;
   error: string | null;
   user: User | null;
+}
+
+export interface GetQuizInitialResponse {
+  ok: boolean;
+  error: string | null;
+  data: Array<QuizInitial> | null;
+}
+
+export interface QuizInitial {
+  quiz: string;
+  Syllables: Array<string>;
 }
 
 export interface Mutation {
